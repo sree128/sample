7c8b4edefdc2e6a6b854a633966c000f27f6bf34
SreelathaKanchireddy <105054104+SreelathaKanchireddy@users.noreply.github.com>
2022-12-27 15:45:39 +0530

    add new view controller

--- sample/SecondViewController.swift
+++ sample/SecondViewController.swift
@@ -11,7 +11,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
     
